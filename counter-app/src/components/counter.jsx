f45941dfbb86b8ea9c932eb0b13b08383b256d40
laxmanbalaraman <laxmanbalaraman@gmail.com>
2022-01-13 15:49:01 +0530

    Add multiple counters Add multiple counters and each counter has delete button. The reset button now doesn't work because  of issue in single source of truth.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,115 +1,136 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Counter extends Component {
-
-    /* 
+  /* 
         state is a react object that contains the dynamic object 
         that this component needs
     */
 
-    state = {
-        count: 0, 
-        tags: ["tag1", "tag2", "tag3"]
-    }
+  state = {
+    count: this.props.counter.value,
+    tags: ["tag1", "tag2", "tag3"],
+  };
 
-    // dont keep this inside state because myStyles is a static data object and doesn't required to be inside state 
+  // dont keep this inside state because myStyles is a static data object and doesn't required to be inside state
 
-    myStyles = {
-        fontSize: 15,
-        fontWeight: 'bold'
-    }
+  myStyles = {
+    fontSize: 15,
+    fontWeight: "bold",
+  };
 
-    // bind the event handler with the object
+  // bind the event handler with the object
 
-    constructor(){
-        super();
-        // this in constructor represents the object
-        console.log("constructor", this);
-        this.handleIncrement = this.handleIncrement.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    // this in constructor represents the object
+    console.log("constructor", this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
 
-    // another way to avoid 'this' is to make define the class methods using arrow syntax
-    // as arrow function inherits the class and doesn't rebind (experimental feature)
+  // another way to avoid 'this' is to make define the class methods using arrow syntax
+  // as arrow function inherits the class and doesn't rebind (experimental feature)
 
-    handleIncrement(id) {
-        // this will return undefined unless binded with the object
-        console.log("Increment button clicked!", this);
-        
-        // setState is used to change dynamic values of state object. it simply see the difference and add or merges this object 
-        // on top of the previous state object. 
-        this.setState(
-            { count: this.state.count + 1 }
-        );
-        // This will be Asynchronous
-        console.log("id is", id);
-        console.log(this.state.count);
-    };
+  handleIncrement(id) {
+    // this will return undefined unless binded with the object
+    console.log("Increment button clicked!", this);
 
-    handleDecrement(id) {
-        this.setState(
-            {count: this.state.count - 1}
-        )
-    };
-    /*
+    // setState is used to change dynamic values of state object. it simply see the difference and add or merges this object
+    // on top of the previous state object.
+    this.setState({ count: this.state.count + 1 });
+    // This will be Asynchronous
+    console.log("id is", id);
+    console.log(this.state.count);
+  }
+
+  handleDecrement(id) {
+    console.log("id is", this.props.counter.id);
+    this.setState({ count: this.state.count - 1 });
+  }
+  /*
         The render method must return only one parent element, 
         enclose the jsx inside a div tag or <React.Fragment></React.Fragment>
         if you do not want the parent tag to be rendered. 
     */
 
-    render() {
-        // { can take any valid Js expresion } {} -> act like an echo
-        /*  class attribute is className in React because class is a reserved keyword in JavaScript 
+  render() {
+    // { can take any valid Js expresion } {} -> act like an echo
+    /*  class attribute is className in React because class is a reserved keyword in JavaScript 
             and since we use JSX in React which itself is the extension of JavaScript, so we have to use 
             className instead of class attribute
         */
-       console.log("Render", this);
-        return( 
+    console.log("props", this.props.counter.value);
+    console.log("Render", this);
+    return (
+      <div>
+        {/* items inside the component tags (childre) are accesssed using props. */}
+        {this.props.children}
 
+        {/* <span>{ this.state.count }</span> */}
+        <span style={this.myStyles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
 
-        <div>
-            {/* <span>{ this.state.count }</span> */}
-            <span style={ this.myStyles } className = { this.getBadgeClasses() } >{ this.formatCount() }</span>
-
-            {/* to pass arguments in the function we need to used the arrow function which returns our fuction with arguments as
+        {/* to pass arguments in the function we need to used the arrow function which returns our fuction with arguments as
             the onclick attribute only accetps function reference which is what is done by arrow function (basically a function
             returning our indetended function). */}
 
-            <button onClick={() => this.handleIncrement(1)} className="btn btn-secondary btn-sm">Increment</button>
-            <button onClick={() => this.handleDecrement(1)} className="btn btn-secondary btn-sm m-2">Decrement</button>
+        <button
+          onClick={() => this.handleIncrement(1)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.handleDecrement(1)}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Decrement
+        </button>
 
-            {/* redering lists */}
+        <button
+          onClick={() => this.props.handleDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
 
-            <ul>
+        {/* redering lists */}
 
-            {/* React uses the key prop to create a relationship between the component and the DOM element. 
+        {/* <ul> */}
+
+        {/* React uses the key prop to create a relationship between the component and the DOM element. 
             If the key is an index, reordering an item in the array changes it. 
             Then React will get confused and re-render the incorrect element. Dont use index as key*/}
 
-                { this.state.tags.map(tag => <li key={ tag } >{ tag }</li>) }
-                {/* by concept of truthy and falsey: see bottom of page for more info */}
-                { this.state.tags.length === 0 && "Please provide some tags!" }
+        {/* { this.state.tags.map(tag => <li key={ tag } >{ tag }</li>) } */}
+        {/* by concept of truthy and falsey: see bottom of page for more info */}
+        {/* { this.state.tags.length === 0 && "Please provide some tags!" } */}
 
-            </ul>
+        {/* </ul> */}
+      </div>
+    );
+  }
 
-        </div>
-        );
-    }
+  formatCount() {
+    // object destructuring: const { count: ct, var2, ... } = this.state
+    // is same as const ct = this.state.count, ...
 
-    formatCount() {
-        // object destructuring: const { count: ct, var2, ... } = this.state 
-        // is same as const ct = this.state.count, ...
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count; 
-    }
-
-    // change badge color dynamically
-    getBadgeClasses(){
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : this.state.count < 0 ? "danger" : "primary";
-        return classes;
-    }
+  // change badge color dynamically
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes +=
+      this.state.count === 0
+        ? "warning"
+        : this.state.count < 0
+        ? "danger"
+        : "primary";
+    return classes;
+  }
 }
- 
+
 export default Counter;
 
 /*
---
+++ counter-app/src/components/counters.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (id) => {
+    // console.log("handleDelete called", this.state.counters);
+    const counters = this.state.counters.filter((counter) => counter.id !== id);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((counter) => {
+      counter.value = 0;
+    });
+    console.log(this.state.counters);
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleReset} className="btn btn-primary btn-sm">
+          Reset
+        </button>
+        {this.state.counters.map((counter) => (
+          // tags inside component is called childeren and they can be accesssed using props
+
+          <Counter
+            key={counter.id}
+            // good practice to send the entire object as attribute value to reduce space
+            // value={counter.value}
+            // id={counter.id}
+            handleDelete={this.handleDelete}
+            counter={counter}
+          >
+            {/* can be accesesed using this.props.children */}
+            <h6>Counter {counter.id}</h6>
+          </Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
