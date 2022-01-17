0d056b8c6c1e3af1573c94f1b1162b75e0a2064a
laxmanbalaraman <laxmanbalaraman@gmail.com>
2022-01-17 10:20:42 +0530

    Add navbar with each links routed.

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,11 +1,27 @@
-import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/navbar";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import notFound from "./components/notFound";
+import Rentals from "./components/rentals";
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/customers" component={Customers} />
+          <Route path="/not-found" component={notFound} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/movies" component={Movies} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
