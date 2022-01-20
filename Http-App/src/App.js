fa21258ec3663d60d289c5d9a16169c773d6f541
laxmanbalaraman <laxmanbalaraman@gmail.com>
2022-01-20 20:24:34 +0530

    Perform CRUD operations using axios.

--- Http-App/src/App.js
+++ Http-App/src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from "react";
-import config from "./config.json";
+import axios from "axios";
 import "./App.css";
 
+const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 class App extends Component {
   state = {
     posts: [],
   };
 
-  handleAdd = () => {
-    console.log("add");
+  async componentDidMount() {
+    // returns a promise which is an object represents the eventual completion
+    // (or failure) of an asynchronous operation and its resulting value.
+    // if u use await then decoratie the function with async.
+    const { data: posts } = await axios.get(apiEndpoint);
+    this.setState({ posts });
+  }
+
+  handleAdd = async () => {
+    const obj = { title: "New post", body: "This is a new post" };
+    const { data: post } = await axios.post(apiEndpoint, obj);
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
   };
 
-  handleUpdate = (post) => {
-    console.log("update");
+  handleUpdate = async (post) => {
+    post.title = "UPDATED";
+    await axios.put(apiEndpoint + "/" + post.id, post);
+    const posts = [...this.state.posts];
+    const index = posts.indexOf(post);
+    posts[index] = { ...post };
+    this.setState({ posts });
   };
 
-  handleDelete = (post) => {
-    console.log("delete");
+  handleDelete = async (post) => {
+    await axios.delete(apiEndpoint + "/" + post.id);
+    const posts = this.state.posts.filter((p) => p.id !== post.id);
+    this.setState({ posts });
   };
 
   render() {
