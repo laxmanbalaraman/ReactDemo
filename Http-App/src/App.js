e38b72c4f5177dd13d7609b6794f374878a70924
laxmanbalaraman <laxmanbalaraman@gmail.com>
2022-01-20 17:21:06 +0530

    Http-App intial commit.

---
+++ Http-App/src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import config from "./config.json";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  handleAdd = () => {
+    console.log("add");
+  };
+
+  handleUpdate = (post) => {
+    console.log("update");
+  };
+
+  handleDelete = (post) => {
+    console.log("delete");
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <button className="btn btn-primary" onClick={this.handleAdd}>
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map((post) => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-info btn-sm"
+                    onClick={() => this.handleUpdate(post)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(post)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
